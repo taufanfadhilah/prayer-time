6558ae889fdb091f23adedbc40cfafb239ca68ca
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2025-12-16 15:32:03 +0700

    fix: correct layout for TV portrait mode by reintroducing wrapper div

--- src/App.jsx
+++ src/App.jsx
@@ -379,8 +379,7 @@ function App() {
   }, [prepared]);
 
   return (
-    // <div className="tv-portrait-wrapper bg-dark-background">
-    <div className="bg-dark-background">
+    <div className="tv-portrait-wrapper bg-dark-background">
       <div className="w-full h-full flex">
         {/* Main Card Container */}
         <div className="prayer-card-container w-full h-full">
